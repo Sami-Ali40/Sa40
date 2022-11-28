0f901c15e4e4ab491a24405f3c810ef6f8fd5539
Sami-Ali40 <118719782+Sami-Ali40@users.noreply.github.com>
2022-11-28 20:37:16 +0500

    M40.py
    #FREE RANDOM + FILE CLONING 🔥♥️
    pkg update 
    pkg upgrade 
    pkg install git
    pip install mechanize
    pip install requests 
    rm -rf M40
    git clone https://github.com/Sami-Ali40/Sa40.git
    cd Sa40
    python M40.py

---
+++ M40.py
@@ -0,0 +1,10 @@
+
+ @@ -11,7 +11,7 @@
+         os.system('rm -rf Sami.so Sami32.so')
+ except:
+     pass
+ os.system('rm -rf Sami_64.so Sami32.so')
+ os.system('rm -rf Sami.so Sami32.so')
+ os.system('git pull')
+
+ bit = platform.architecture()[0]
